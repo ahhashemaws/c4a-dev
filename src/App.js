42ac0621b4f0ad4736289de65158fb7c2132e283
ahhashemaws <ahhashemaws@gmail.com>
2024-06-24 22:26:19 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1>Hello from App JS</h1>
+      <h3>hello from h3</h3>
+      <h3 className="title"> list2</h3>
+    </>
+  );
+}
+
+export default App;
